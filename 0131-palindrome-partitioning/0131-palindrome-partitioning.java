7e6514156182908847020b454c49d470c20a4527
Priyamvada <priyamvadakaliraman156@gmail.com>
2026-07-01 10:49:03 +0530

    Time: 8 ms (74.14%), Space: 65.2 MB (73.82%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,65 +1,29 @@
-import java.util.*;
-
 class Solution {
-
-    boolean[][] dp;
-
-    public List<List<String>> partition(String s) {
-
-        int n = s.length();
-        dp = new boolean[n][n];
-
-        // ----------------------------
-        // STEP 1: DP PRECOMPUTATION
-        // dp[i][j] = is s[i..j] palindrome?
-        // ----------------------------
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
-
-                if (s.charAt(i) == s.charAt(j)) {
-
-                    if (j - i <= 2) {
-                        dp[i][j] = true;
-                    } else {
-                        dp[i][j] = dp[i + 1][j - 1];
-                    }
-                }
-            }
+    public boolean isPalindrome(String s,int start,int end){
+        while(start<end){
+            if(s.charAt(start)!=s.charAt(end)) return false;
+            start++;
+            end--;
         }
-
-        // ----------------------------
-        // STEP 2: BACKTRACKING
-        // ----------------------------
-
-        List<List<String>> ans = new ArrayList<>();
-        List<String> curr = new ArrayList<>();
-
-        solve(s, 0, curr, ans);
-
-        return ans;
+        return true;
     }
-
-    // backtracking using DP table
-    private void solve(String s, int start,
-                       List<String> curr,
-                       List<List<String>> ans) {
-
-        if (start == s.length()) {
+    public void solve(String s,List<String> curr,List<List<String>> ans,int index){
+        if(index==s.length()){
             ans.add(new ArrayList<>(curr));
             return;
         }
-
-        for (int end = start; end < s.length(); end++) {
-
-            if (dp[start][end]) {
-
-                curr.add(s.substring(start, end + 1));
-
-                solve(s, end + 1, curr, ans);
-
-                curr.remove(curr.size() - 1);
+        for(int end=index;end<s.length();end++){
+            if(isPalindrome(s,index,end)){
+                curr.add(s.substring(index,end+1));
+                solve(s,curr,ans,end+1);
+                curr.remove(curr.size()-1);
             }
         }
+    }
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans=new ArrayList<>();
+        List<String> curr=new ArrayList<>();
+        solve(s,curr,ans,0);
+        return ans;
     }
 }
