f134599bfc03560f221f916912eedaf67b32a4ab
Priyamvada <priyamvadakaliraman156@gmail.com>
2026-07-04 23:44:41 +0530

    Time: 129 ms (80.51%), Space: 42.9 MB (75.15%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,30 +1,47 @@
 class Solution {
-    private boolean dfs(int i,int j,int index,char[][] board,String word,boolean[][] vis){
-        if(index==word.length()) return true;
-        if(i<0 || j<0 ||i>=board.length || j>=board[0].length) return false;
-        if(vis[i][j]==true) return false;
-        if(board[i][j]!=word.charAt(index)) return false;
-        vis[i][j]=true;
-        boolean down=dfs(i+1,j,index+1,board,word,vis);
-        boolean up=dfs(i-1,j,index+1,board,word,vis);
-        boolean right=dfs(i,j+1,index+1,board,word,vis);
-        boolean left=dfs(i,j-1,index+1,board,word,vis);
-        if(down || up || right|| left){
-        vis[i][j]=false;
-        return true;
-        }
-        vis[i][j]=false;
-        return false;
+
+    private boolean dfs(int i, int j, int index, char[][] board, String word) {
+
+        if (index == word.length())
+            return true;
+
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length)
+            return false;
+
+        if (board[i][j] != word.charAt(index))
+            return false;
+
+        char temp = board[i][j];
+        board[i][j] = '#';          // mark visited
+
+        boolean found =
+                dfs(i + 1, j, index + 1, board, word) ||
+                dfs(i - 1, j, index + 1, board, word) ||
+                dfs(i, j + 1, index + 1, board, word) ||
+                dfs(i, j - 1, index + 1, board, word);
+
+        board[i][j] = temp;         // restore (backtrack)
+
+        return found;
     }
+
     public boolean exist(char[][] board, String word) {
-        boolean[][] vis = new boolean[board.length][board[0].length];
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[i].length;j++){
-                if(board[i][j]==word.charAt(0)){
-                    if(dfs(i,j,0,board,word,vis)) return true;
+
+        int m = board.length;
+        int n = board[0].length;
+        if(word.length()>n*m) return false;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (board[i][j] == word.charAt(0)) {
+                    if (dfs(i, j, 0, board, word))
+                        return true;
                 }
+
             }
         }
-return false;
+
+        return false;
     }
 }
