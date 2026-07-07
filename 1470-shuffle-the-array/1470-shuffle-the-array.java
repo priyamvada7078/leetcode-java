86943ea60e8422c39bff9266657b8670a8e69f53
Priyamvada <priyamvadakaliraman156@gmail.com>
2026-07-07 15:40:22 +0530

    Time: 1 ms (13.09%), Space: 46.4 MB (80.44%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,17 +1,23 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
 
-        int[] ans = new int[2 * n];
-
-        int i = 0;
-        int j = n;
-        int k = 0;
-
-        while (i < n && j < 2 * n) {
-            ans[k++] = nums[i++];
-            ans[k++] = nums[j++];
+        // Store y-values in x-values
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] | (nums[i + n] << 10);
         }
 
-        return ans;
+        int index = 2 * n - 1;
+
+        // Decode from back
+        for (int i = n - 1; i >= 0; i--) {
+
+            int x = nums[i] & 1023;      // lower 10 bits
+            int y = nums[i] >> 10;       // higher bits
+
+            nums[index--] = y;
+            nums[index--] = x;
+        }
+
+        return nums;
     }
 }
