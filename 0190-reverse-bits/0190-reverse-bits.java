86bdc505c3a0e4ef394d1b6f75d3c678717bb7d4
Priyamvada <priyamvadakaliraman156@gmail.com>
2026-07-11 20:26:13 +0530

    Time: 1 ms (61.99%), Space: 42.7 MB (9.35%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,11 +1,25 @@
-public class Solution {
+class Solution {
     public int reverseBits(int n) {
-        int res = 0;
-        for (int i = 0; i < 32; i++) {
-            res <<= 1;
-            res |= (n & 1);
-            n >>>= 1;
+
+        int left = 0;
+        int right = 31;
+
+        while (left < right) {
+
+            // Extract bits at left and right positions
+            int leftBit = (n >> left) & 1;
+            int rightBit = (n >> right) & 1;
+
+            // If bits are different, swap them
+            if (leftBit != rightBit) {
+                n ^= (1 << left);
+                n ^= (1 << right);
+            }
+
+            left++;
+            right--;
         }
-        return res;
+
+        return n;
     }
 }
