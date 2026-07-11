c9ea5167e91040b988bfb6c7cdaa0d91f5f603cd
Priyamvada <priyamvadakaliraman156@gmail.com>
2026-07-11 20:41:05 +0530

    Time: 2 ms (96.36%), Space: 48.7 MB (50.75%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,21 +1,27 @@
 class Solution {
     public int[] countBits(int n) {
-
-        int[] arr = new int[n + 1];
+        int[] ans = new int[n + 1];
 
         for (int i = 0; i <= n; i++) {
 
-            int num = i;
-            int count = 0;
+            if (i == 0) {
+                ans[i] = 0;
+            } else if ((i & (i - 1)) == 0) {
+                // Power of 2
+                ans[i] = 1;
+            } else {
+                int num = i;
+                int count = 0;
 
-            while (num > 0) {
-                count += (num & 1);
-                num >>= 1;
+                while (num > 0) {
+                    count += (num & 1);
+                    num >>= 1;
+                }
+
+                ans[i] = count;
             }
-
-            arr[i] = count;
         }
 
-        return arr;
+        return ans;
     }
 }
