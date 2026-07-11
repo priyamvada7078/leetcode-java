748b997c3b65dd6257e1311e70777571f955c5e8
Priyamvada <priyamvadakaliraman156@gmail.com>
2026-07-11 20:42:37 +0530

    Time: 2 ms (96.36%), Space: 48.8 MB (15.12%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,20 +1,14 @@
 class Solution {
     public int[] countBits(int n) {
+
         int[] ans = new int[n + 1];
-        
-        for (int i = 0; i <= n; i++) {
-            int num = i;  // copy of i
-            int count = 0;
-            
-            // Brian Kernighan’s Algorithm
-            while (num > 0) {
-                num = num & (num - 1); // removes the lowest set bit
-                count++;
-            }
-            
-            ans[i] = count; // store result
+
+        ans[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
         }
-        
+
         return ans;
     }
 }
