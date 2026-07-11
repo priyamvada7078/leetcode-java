b74284e6cf0a1373645384ca0d8fbc4bb747dd47
Priyamvada <priyamvadakaliraman156@gmail.com>
2026-07-11 20:29:35 +0530

    Time: 1 ms (61.99%), Space: 42.3 MB (69.82%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,5 +1,11 @@
-class Solution {
+public class Solution {
     public int reverseBits(int n) {
-        return Integer.reverse(n);
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= (n & 1);
+            n >>>= 1;
+        }
+        return res;
     }
 }
