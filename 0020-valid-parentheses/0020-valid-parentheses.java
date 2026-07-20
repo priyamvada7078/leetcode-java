67469b346a69616beba6ccf4b6321813fee0d57e
Priyamvada <priyamvadakaliraman156@gmail.com>
2026-07-20 17:21:41 +0530

    Time: 3 ms (86.86%), Space: 43.3 MB (58.2%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,28 @@
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
+
         for (int i = 0; i < s.length(); i++) {
             char a = s.charAt(i);
+
             if (a == '(' || a == '{' || a == '[') {
-                st.push(s.charAt(i));
+                st.push(a);
             } else {
                 if (!st.isEmpty() &&
                         ((a == ')' && st.peek() == '(') ||
-                                (a == '}' && st.peek() == '{') ||
-                                (a == ']' && st.peek() == '['))) {
+                         (a == '}' && st.peek() == '{') ||
+                         (a == ']' && st.peek() == '['))) {
+
                     st.pop();
-                } else
+
+                } else {
                     return false;
+                }
             }
         }
-        if(st.isEmpty()) return true;
-        return false;
+
+        return st.isEmpty();
     }
 }
