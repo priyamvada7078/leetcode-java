e3af6aeade6f1d13913526cb3de9ccf0d554d1f0
Priyamvada <priyamvadakaliraman156@gmail.com>
2026-07-27 20:58:54 +0530

    Time: 30 ms (33.38%), Space: 48.1 MB (6.25%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,21 +1,71 @@
-    class Solution {
+class Solution {
+
     public long subArrayRanges(int[] nums) {
         int n = nums.length;
-        long ans = 0;
 
+        int[] prevGreater = new int[n];
+        int[] nextGreater = new int[n];
+        int[] prevSmaller = new int[n];
+        int[] nextSmaller = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        // Previous Greater
         for (int i = 0; i < n; i++) {
-            int min = nums[i];
-            int max = nums[i];
+            while (!st.isEmpty() && nums[st.peek()] <= nums[i])
+                st.pop();
 
-            for (int j = i; j < n; j++) {
-                min = Math.min(min, nums[j]);
-                max = Math.max(max, nums[j]);
-
-                ans += (max - min);
-            }
+            prevGreater[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
         }
 
-        return ans;
+        st.clear();
+
+        // Next Greater
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[st.peek()] < nums[i])
+                st.pop();
+
+            nextGreater[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+
+        // Previous Smaller
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && nums[st.peek()] >= nums[i])
+                st.pop();
+
+            prevSmaller[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+
+        // Next Smaller
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && nums[st.peek()] > nums[i])
+                st.pop();
+
+            nextSmaller[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        long maxSum = 0;
+        long minSum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            long left = i - prevGreater[i];
+            long right = nextGreater[i] - i;
+            maxSum += (long) nums[i] * left * right;
+
+            left = i - prevSmaller[i];
+            right = nextSmaller[i] - i;
+            minSum += (long) nums[i] * left * right;
+        }
+
+        return maxSum - minSum;
     }
 }
-    
