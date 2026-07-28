2b422ade975e9783d79a10dec2fdf3e21d505206
Priyamvada <priyamvadakaliraman156@gmail.com>
2026-07-28 22:45:54 +0530

    Time: 18 ms (79.26%), Space: 50.6 MB (51.13%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,4 +1,4 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
 
@@ -10,68 +10,48 @@ class Solution {
         int rows = matrix.length;
         int cols = matrix[0].length;
 
-        int[] height = new int[cols];
-
+        int[] heights = new int[cols];
         int maxArea = 0;
 
         for (int i = 0; i < rows; i++) {
 
+            // Build histogram
             for (int j = 0; j < cols; j++) {
 
                 if (matrix[i][j] == '1')
-                    height[j]++;
+                    heights[j]++;
                 else
-                    height[j] = 0;
+                    heights[j] = 0;
             }
 
-            maxArea = Math.max(maxArea, largestRectangle(height));
+            // Find largest rectangle in current histogram
+            maxArea = Math.max(maxArea, largestRectangleArea(heights));
         }
 
         return maxArea;
     }
 
-    private int largestRectangle(int[] heights) {
+    private int largestRectangleArea(int[] heights) {
 
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
         int n = heights.length;
 
-        int[] pse = new int[n];
-        int[] nse = new int[n];
+        for (int i = 0; i <= n; i++) {
 
-        Stack<Integer> st = new Stack<>();
+            int currHeight = (i == n) ? 0 : heights[i];
 
-        // Previous Smaller
-        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] > currHeight) {
 
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i])
-                st.pop();
+                int height = heights[stack.pop()];
 
-            pse[i] = st.isEmpty() ? -1 : st.peek();
+                int left = stack.isEmpty() ? -1 : stack.peek();
+                int width = i - left - 1;
 
-            st.push(i);
-        }
+                maxArea = Math.max(maxArea, height * width);
+            }
 
-        st.clear();
-
-        // Next Smaller
-        for (int i = n - 1; i >= 0; i--) {
-
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i])
-                st.pop();
-
-            nse[i] = st.isEmpty() ? n : st.peek();
-
-            st.push(i);
-        }
-
-        int maxArea = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            int width = nse[i] - pse[i] - 1;
-
-            int area = width * heights[i];
-
-            maxArea = Math.max(maxArea, area);
+            stack.push(i);
         }
 
         return maxArea;
